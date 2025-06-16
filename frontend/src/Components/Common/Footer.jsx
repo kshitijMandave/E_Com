543d72157389d6b664a28691bfa27af72a6456cd
kshitijMandave <kshitij.mandwe@gmail.com>
2025-06-16 17:53:17 +0530

    Added Footer Bottom

--- frontend/src/Components/Common/Footer.jsx
+++ frontend/src/Components/Common/Footer.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import { TbBrandMeta } from "react-icons/tb";
+import { IoLogoInstagram } from "react-icons/io";
+import { FaXTwitter } from "react-icons/fa6";
+import { FiPhoneCall } from "react-icons/fi";
+
 import { Link } from "react-router-dom";
 
 function Footer() {
@@ -61,7 +66,7 @@ function Footer() {
         </div>
         {/*Support links */}
         <div>
-          <h3 className="text-lg text-gray-800 mb-4">Shop</h3>
+          <h3 className="text-lg text-gray-800 mb-4">Support</h3>
           <ul className="space-y-2 text-gray-600">
             <li>
               <Link to="#" className="hover:text-gray-500 transition-colors">
@@ -86,6 +91,45 @@ function Footer() {
           </ul>
         </div>
         {/* Follow Us */}
+        <div>
+          <h3 className="text-lg text-gray-800 mb-4">Follow Us</h3>
+          <div className="flex items-center space-x-4 mb-6">
+            <a
+              href="https://www.facebook.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-gray-300"
+            >
+              <TbBrandMeta className="h-5 w-5" />
+            </a>
+            <a
+              href="https://www.facebook.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-gray-300"
+            >
+              <IoLogoInstagram className="h-5 w-5" />
+            </a>
+            <a
+              href="https://www.facebook.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-gray-300"
+            >
+              <FaXTwitter className="h-4 w-4" />
+            </a>
+          </div>
+          <p className="text-gray-500">Call Us</p>
+          <p>
+            <FiPhoneCall className="inline-block mr-2" />
+          </p>
+        </div>
+      </div>
+      {/* Footer Bottom*/}
+      <div className="container mx-auto mt-12 px-4 lg:px-0 border-t border-gray-200 pt-6 ">
+        <p className="text-gray-500 text-sm tracking-tighter text-center">
+          © 2025 www.aura.com. All rights reserved.
+        </p>
       </div>
     </footer>
   );
