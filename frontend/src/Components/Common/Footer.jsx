eb21d90ff11fabb3fde16d661af2d0b0afad4a15
kshitijMandave <kshitij.mandwe@gmail.com>
2025-06-16 17:31:13 +0530

    Added Footer Component

---
+++ frontend/src/Components/Common/Footer.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <footer className="border-t py-12">
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-8 px-4 lg:px-0">
+        <div>
+          <h3 className="text-lg text-gray-800 mb-4">Newsletter</h3>
+          <p className="text-gray-500 mb-4">
+            Be the first to hear about new products, exclusive events, and
+            online offers.
+          </p>
+          <p className="font-medium text-sm text-gray-600 mb-6">
+            Sign up and get 10% off your first order.
+          </p>
+
+          {/* Newsletter form */}
+          <form className="flex">
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className="p-3 w-full text-sm border-t border-l border-b border-gray-300 rounded-l-md 
+                     focus:outline-none focus:ring-2 focus:ring-gray-500 transition-all"
+              required
+            />
+            <button
+              type="submit"
+              className="bg-black text-white px-6 py-3 text-sm rounded-r-md hover:bg-gray-800 transition-all cursor-pointer"
+            >
+              Subscribe
+            </button>
+          </form>
+        </div>
+
+        {/*Shop links */}
+        <div>
+          <h3 className="text-lg text-gray-800 mb-4">Shop</h3>
+          <ul className="space-y-2 text-gray-600">
+            <li>
+              <Link to="#" className="hover:text-gray-500 transition-colors">
+                Men
+              </Link>
+            </li>
+            <li>
+              <Link to="#" className="hover:text-gray-500 transition-colors">
+                Women
+              </Link>
+            </li>
+            <li>
+              <Link to="#" className="hover:text-gray-500 transition-colors">
+                Electronics
+              </Link>
+            </li>
+            <li>
+              <Link to="#" className="hover:text-gray-500 transition-colors">
+                Jewellery
+              </Link>
+            </li>
+          </ul>
+        </div>
+        {/*Support links */}
+        <div>
+          <h3 className="text-lg text-gray-800 mb-4">Shop</h3>
+          <ul className="space-y-2 text-gray-600">
+            <li>
+              <Link to="#" className="hover:text-gray-500 transition-colors">
+                Contact Us
+              </Link>
+            </li>
+            <li>
+              <Link to="#" className="hover:text-gray-500 transition-colors">
+                About Us
+              </Link>
+            </li>
+            <li>
+              <Link to="#" className="hover:text-gray-500 transition-colors">
+                FAQs
+              </Link>
+            </li>
+            <li>
+              <Link to="#" className="hover:text-gray-500 transition-colors">
+                Features
+              </Link>
+            </li>
+          </ul>
+        </div>
+        {/* Follow Us */}
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- frontend/src/Components/Layout/UsreLayout.jsx
+++ frontend/src/Components/Layout/UsreLayout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../Common/Header";
+import Footer from "../Common/Footer";
 
 function UsreLayout() {
   return (
@@ -8,6 +9,7 @@ function UsreLayout() {
       <Header />
       {/*Main Content*/}
       {/*Footer*/}
+      <Footer />
     </div>
   );
 }
