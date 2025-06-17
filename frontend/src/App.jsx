acf289476670c1555295d32d69a0f3049f4312b1
kshitijMandave <kshitij.mandwe@gmail.com>
2025-06-17 18:00:37 +0530

    added sonner library for the notifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,12 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UsreLayout from "./Components/Layout/UsreLayout";
 import Home from "./Pages/Home";
+import { Toaster } from "sonner";
 
 function App() {
   return (
     <BrowserRouter>
+      <Toaster position="top-right" />
       <Routes>
         <Route path="/" element={<UsreLayout />}>
           <Route index element={<Home />}></Route>
