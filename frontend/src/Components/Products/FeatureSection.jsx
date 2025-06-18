2faa2d6f4055bab6d4cb5d0664c3746e943474ca
kshitijMandave <kshitij.mandwe@gmail.com>
2025-06-18 15:04:04 +0530

    Modified FeaturedCollection component

---
+++ frontend/src/Components/Products/FeatureSection.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FeatureSection() {
+  return <div>FeatureSection</div>;
+}
+
+export default FeatureSection;
--- frontend/src/Components/Products/FeaturedCollection.jsx
+++ frontend/src/Components/Products/FeaturedCollection.jsx
@@ -1,22 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import featuredCollection from "./images/FeaturedCollection.jpg";
 
 function FeaturedCollection() {
   return (
-    <section className="py-16 px-4 lg:px-0">
-      <div className="container mx-auto flex flex-col-reverse lg:flex-row items-center bg-green-50 rounded-3xl">
-        {/*Left content */}
-        <div className="lg:w-1/2 p-8 text-center lg:text-left">
-          <h2 className="text-lg font-semibold text-gray-700 mb-2">
+    <section className="py-20 px-4 lg:px-0">
+      <div className="container mx-auto flex flex-col-reverse lg:flex-row items-center bg-green-50 rounded-3xl  overflow-hidden transition-all duration-300">
+        {/* Left Content */}
+        <div className="lg:w-1/2 p-10 text-center lg:text-left animate-fade-in">
+          <h2 className="text-base uppercase font-semibold text-green-700 mb-3 tracking-wide">
             Not Just Apparel, It's a Lifestyle
           </h2>
-          <h2 className="text-4xl lg:text-5xl font-bold mb-6">
+          <h2 className="text-4xl lg:text-5xl font-bold text-gray-900 mb-6 leading-tight">
             Redefining Everyday Fashion
           </h2>
-          <p className="text-lg text-gray-600 mb-6">
+          <p className="text-lg text-gray-700 mb-8 max-w-xl">
             From chill weekends to bold weekdays explore essentials that offer
             the perfect balance between laid-back comfort and street-smart
-            style. It's more than fashion; it’s how you live.
+            style. It's more than fashion; it’s how you live. Step into everyday
+            luxury.
           </p>
+          <Link
+            to="/collection/all"
+            className="inline-block bg-black text-white px-6 py-3 rounded-lg text-lg hover:bg-gray-800 transition-colors duration-300"
+          >
+            Shop Now
+          </Link>
+        </div>
+
+        {/* Right Content (Image) */}
+        <div className="lg:w-1/2 h-80 lg:h-[500px] w-full">
+          <img
+            src={featuredCollection}
+            alt="Featured Collection"
+            className="w-full h-full object-cover object-center lg:rounded-tr-3xl lg:rounded-br-3xl"
+          />
         </div>
       </div>
     </section>
