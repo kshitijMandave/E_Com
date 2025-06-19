b8e1023dc382a2b3f9cc10ef04f4276ae109b84d
kshitijMandave <kshitij.mandwe@gmail.com>
2025-06-19 22:46:44 +0530

    Modified Register Component to print the entered value on console and tested

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -8,11 +8,19 @@ function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("User Registered : ", { firstName, lastName, email, password });
+  };
+
   return (
     <div className="flex h-screen">
       {/* Left side - Form */}
       <div className="w-full md:w-1/2 flex flex-col justify-center items-center p-8 md:p-12">
-        <form className="w-full max-w-md bg-white p-8 rounded-lg border shadow-sm">
+        <form
+          onSubmit={handleSubmit}
+          className="w-full max-w-md bg-white p-8 rounded-lg border shadow-sm"
+        >
           <div className="flex justify-center mb-6">
             <h2 className="text-xl font-medium">Aura</h2>
           </div>
