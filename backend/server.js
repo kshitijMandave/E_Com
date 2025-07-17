fd022b166de610c2a64bcd81d4159b78a177235d
kshitijMandave <kshitij.mandwe@gmail.com>
2025-07-17 14:35:31 +0530

    Created server.js to configure Express server, MongoDB connection, and dotenv setup. Implemented base route ('/') with a welcome message. Server successfully listens on the specified PORT.

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+// Importing the Express framework to build web server and handle backend routes.
+
+const cors = require("cors");
+// Importing the CORS middleware to allow cross-origin requests from different domains.
+
+const dotenv = require("dotenv");
+// Importing dotenv package to load environment variables from a .env file
+
+const connectDB = require("./config/db");
+
+const app = express();
+// Creating an instance of the Express application
+
+/* use() is a function in Express that is used to handle and apply middleware. like : (req, res, next) */
+app.use(express.json());
+// Middleware to parse incoming JSON data in request bodies
+
+app.use(cors());
+// Middleware to enable Cross-Origin Resource Sharing (CORS) for all incoming requests
+
+dotenv.config(); // Loads variables from .env into process.env
+
+// console.log(process.env.PORT);
+const PORT = process.env.PORT || 3000; // now here we are accessing the values like : PORT
+// here we are giving the defualt value 3000
+
+// Connect to DB
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Hi!! welcome to Aura API");
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on http://localhost/${PORT}`);
+});
