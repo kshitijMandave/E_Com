f4233d28e87667ada51d18262e339bbea4e9729a
kshitijMandave <kshitij.mandwe@gmail.com>
2025-08-13 23:35:32 +0530

    Added the Route to uodate the Product based on existing ID, where we can update the details of product

--- backend/routes/ProductRoutes.js
+++ backend/routes/ProductRoutes.js
@@ -70,4 +70,79 @@ router.post("/", protect, admin, async (req, res) => {
   }
 });
 
+// @route PUT /api/products/:id
+// @desc Update a product with existing ID
+// @access Private/Admin
+router.put("/:id", protect, admin, async (req, res) => {
+  try {
+    const {
+      name,
+      description,
+      price,
+      discountPrice,
+      countInStock,
+      sku,
+      category,
+      brand,
+      size,
+      colors,
+      collection,
+      material,
+      gender,
+      images,
+      isFeatured,
+      isPublished,
+      rating,
+      numReviews,
+      tags,
+      metaTitle,
+      metaDescription,
+      metaKeywords,
+      dimensions,
+      weight,
+    } = req.body;
+
+    // Find the product by ID
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      product.name = name || product.name;
+      product.description = description || product.description;
+      product.price = price || product.price;
+      product.discountPrice = discountPrice || product.discountPrice;
+      product.countInStock = countInStock || product.countInStock;
+      product.sku = sku || product.sku;
+      product.category = category || product.category;
+      product.brand = brand || product.brand;
+      product.size = size || product.size;
+      product.colors = colors || product.colors;
+      product.collection = collection || product.collection;
+      product.material = material || product.material;
+      product.gender = gender || product.gender;
+      product.images = images || product.images;
+      product.isFeatured =
+        isFeatured !== undefined ? isFeatured : product.isFeatured;
+      product.isPublished =
+        isPublished !== undefined ? isPublished : product.isPublished;
+      product.rating = rating || product.rating;
+      product.numReviews = numReviews || product.numReviews;
+      product.tags = tags || product.tags;
+      product.metaTitle = metaTitle || product.metaTitle;
+      product.metaDescription = metaDescription || product.metaDescription;
+      product.metaKeywords = metaKeywords || product.metaKeywords;
+      product.dimensions = dimensions || product.dimensions;
+      product.weight = weight || product.weight;
+
+      // Save the updated product
+      const updatedProduct = await product.save();
+      res.status(200).json(updatedProduct);
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
 module.exports = router;
