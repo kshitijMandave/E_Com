38ba182a4eae31a5add86b6f6be4be2b7a975f13
kshitijMandave <kshitij.mandwe@gmail.com>
2025-08-14 00:22:13 +0530

    Created the seeder file where added 20 documents (20 products). and created the script to seed products and into MongoDB

---
+++ backend/seeder.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const dotenve = require("dotenv");
+const Product = require("./models/Product");
+const User = require("./models/User");
+const products = require("./products");
+
+dotenve.config();
+
+// connect to MongoDB
+mongoose.connect(process.env.MONGO_URI);
+
+// function to seed the data
+
+const seedData = async () => {
+  try {
+    // Clear existing data
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    // Create default user
+    const createdUser = new User({
+      name: "Admin",
+      email: "admin@example.com",
+      password: "admin123",
+      role: "admin",
+    });
+
+    //Assign the default user ID to each product
+    const userID = createdUser._id;
+
+    const samleProducts = products.map((product) => {
+      return { ...product, user: userID };
+    });
+
+    // Insert the products into the database
+    await Product.insertMany(samleProducts);
+
+    console.log("Data seeded successfully");
+    process.exit();
+  } catch (error) {
+    console.error("Error seeding data:", error);
+    process.exit(1);
+  }
+};
+
+seedData();
