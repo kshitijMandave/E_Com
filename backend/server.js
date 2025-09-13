faddc771e9d2b2e18fbef5ee590c3b3822fd88da
kshitijMandave <kshitij.mandwe@gmail.com>
2025-09-13 22:27:15 +0530

    added OrderRoutes for fetching user orders and order details

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ const UserRoutes = require("./routes/UserRoutes");
 const ProductRoutes = require("./routes/ProductRoutes");
 const CartRoutes = require("./routes/CartRoutes");
 const CheckoutRoutes = require("./routes/CheckoutRoutes");
+const OrderRoutes = require("./routes/OrderRoutes");
 
 const app = express();
 // Creating an instance of the Express application
@@ -42,6 +43,7 @@ app.use("/api/users", UserRoutes);
 app.use("/api/products", ProductRoutes);
 app.use("/api/cart", CartRoutes);
 app.use("/api/checkout", CheckoutRoutes);
+app.use("/api/orders", OrderRoutes);
 
 app.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}`);
