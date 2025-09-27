b99bf9ac54c0c94ed3007e3765f24872b7e29e45
kshitijMandave <kshitij.mandwe@gmail.com>
2025-09-28 00:08:26 +0530

    added checkoutSlice with async thunk for creating checkout session

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import productReducer from "./slices/producstSlice";
+import cartReducer from "./slices/cartSlice";
+import checkoutReducer from "./slices/checkoutSlice";
 const store = configureStore({
   reducer: {
     auth: authReducer,
     products: productReducer,
+    cart: cartReducer,
+    checkout: checkoutReducer,
   },
 });
 
